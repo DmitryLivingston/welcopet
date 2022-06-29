c9414ea00ac535e3161207f74e1bdf89a83308d1
DmitryLivingston <livingston.dd92@gmail.com>
2022-06-29 19:01:30 +0300

    Модалка

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-var myModal = new bootstrap.Modal(document.getElementById('modalMenu'));
-const close = document.querySelector(".btn-close");
-const why = document.querySelector("#modal-menu-why");
-close.onclick = ()=>console.log("item-text clicked!");
-// why.addEventListener("click",()=>myModal.hide());
-why.addEventListener("click",()=>window.location.hash="#about");
-why.addEventListener('click', function(e) { 
-var aboutScrollBtn = document.querySelector("#about-scroll-btn");
+// var myModal = new bootstrap.Modal(document.getElementById('modalMenu'));
+// const close = document.querySelector(".btn-close");
+// const why = document.querySelector("#modal-menu-why");
+// close.onclick = ()=>console.log("item-text clicked!");
+// // why.addEventListener("click",()=>myModal.hide());
+// why.addEventListener("click",()=>window.location.hash="#about");
+// why.addEventListener('click', function(e) { 
+// var aboutScrollBtn = document.querySelector("#about-scroll-btn");
 
-    myModal.hide();
-    aboutScrollBtn.click();
-    aboutScrollBtn.onclick = ()=>console.log("item-text clicked!");
-})
+//     myModal.hide();
+//     aboutScrollBtn.click();
+//     aboutScrollBtn.onclick = ()=>console.log("item-text clicked!");
+// })
+// $('#modalMenuWhy').click(function(){
+//     $('#modalMenu').modal('hide');
+//     console.log('click');
+//     $(document.body).animate({
+//         'scrollTop':   $('#about').offset().top
+//     }, 2000);
+// });
+// .my-anchor = the class of the trigger
